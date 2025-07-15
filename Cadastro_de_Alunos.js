807df0b209d5a5a63dacd180402cd49c710703ce
LipeFB <felipeboth12@gmail.com>
2025-07-15 19:29:50 -0300

    Atualização pré-apresentação

--- Cadastro_de_Alunos.js
+++ Cadastro_de_Alunos.js
@@ -1,25 +1,25 @@
 // Desafio - Cadastro de Aluno
 
-// Mensagem de inicializacao
+// Mensagem de boas-vindas
 console.log(`
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 Seja Bem-vindo(a) ao SCA - Sistema de Cadastro de Alunos
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-`)
+`);
 
 
 
-// Objeto -> Aluno, com todas as informacoes
+// Objeto aluno com as propriedades
 const aluno = {
   nome: "Felipe",
   idade: 17,
-  curso: "Ensino Medio",
-  notas: [10, 9, 8]
+  curso: "Ensino Médio",
+  notas: [0, 0, 0]
 };
 
 
-// Mostra na tela as informacoes sobre o(a) aluno(a)
-console.log(`Informacoes Cadastrais:
+// Mostra os dados do aluno
+console.log(`Informações Cadastrais:
 
 Nome:  ${aluno.nome}
 Idade: ${aluno.idade}
@@ -29,28 +29,29 @@ Notas: ${aluno.notas.join(" | ")}
 `);
 
 
-// Funcao que calcula e mostra na tela a media do(a) aluno(a) e indica se passou ou nao
+// Funcao que calcula a media e mostra o resultado
 function calculoMedia (media) {
-  media = (aluno.notas[0] + aluno.notas[1] + aluno.notas[2]) / 3 ;   // Calculo da Media
+  media = (aluno.notas[0] + aluno.notas[1] + aluno.notas[2]) / 3 ;
 
-  let resultado = media >= 7 ? "APROVADO(A)" : "REPROVADO(A)";       // Verifica se foi aprovado
+  let resultado = media >= 7 ? "APROVADO(A)" : "REPROVADO(A)";
   
-  mediaFormatada = media.toFixed(1)
+  mediaFormatada = media.toFixed(1);
 
   console.log(`Resultado:
 
-O(a) aluno(a) ${aluno.nome}, atingiu a media anual de ${mediaFormatada} pontos, portanto foi 
+O(a) aluno(a) ${aluno.nome}, atingiu a média anual de ${mediaFormatada} pontos, portanto foi 
 
 --------------------------- ${resultado} ---------------------------
-`)
+`);
 
-  if (resultado = "Aprovado(a)") {
+  if (resultado === "APROVADO(A)") {
     console.log(`
-*A equipe SCA parabeniza o estudante pela sua conquista!`)
+*A equipe SCA parabeniza o(a) estudante por sua conquista!`);
   } else {
     console.log(`
-*A equipe SCA lamenta a reprovacao do(a) estudante`)
+*A equipe SCA lamenta a reprovação do(a) estudante`);
   }
 }
 
-calculoMedia()
+// Executar função
+calculoMedia();
